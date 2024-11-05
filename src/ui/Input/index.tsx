51ff9aa3f89718bd0618254ef64ad88fa8b8ea3f
Victor K <dev@victorkarasev.ru>
2024-11-06 03:50:33 +0700

    fix

--- src/ui/Input/index.tsx
+++ src/ui/Input/index.tsx
@@ -16,7 +16,7 @@ const Input: FC<TInputProps> = (props) => {
   return (
     <input
       {...props}
-      value={+(props.value ?? 0)}
+      value={props.value ?? ''}
       className='input'
       onChange={onChangeHandler}
     />
