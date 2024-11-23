6270f25a627682545240858a1c4bfc79ed1039dd
Victor K <dev@victorkarasev.ru>
2024-11-23 18:44:58 +0700

    v2.2.6d
    recolor range heatmap

--- src/components/RangeTable/index.tsx
+++ src/components/RangeTable/index.tsx
@@ -1,30 +1,43 @@
 import { FC } from 'react';
 import './styles.css';
 
+const COLOR_RATIO = 1/700*360;
+
 const Data = () => {
   return new Array(10).fill(null).map((_, rowIndex) => 
-    new Array(15).fill(0).map((_, colIndex) => {
-        const line = Math.round(Math.sqrt( (rowIndex*50)**2 + (colIndex*50)**2 ));
-        return !(line > 700 || (line > 50 && line < 100)) ? line : 0;
+    new Array(14).fill(null).map((_, colIndex) => {
+        const value = Math.round(Math.sqrt( (rowIndex*50)**2 + ((colIndex && colIndex+1)*50)**2 ));
+        return !(value > 700) ? value : 0;
     })
   )
 };
 
-const COLOR_RATIO = 1/700*360;
+const GetColor = (value: number, colIdx: number, rowIdx: number) => {
+  if (!rowIdx || !value) return;
+
+  return `hsl(
+    ${value * COLOR_RATIO},
+    100%,
+    75%,
+    ${rowIdx < colIdx-1 ? '.35' : 1}
+  )`;
+};
 
 const RangeTable:FC = () => (
   <div>
     <hr />
     <div>Range heatmap:</div>
     <div className='table'>
-      {Data().map((line, idx) => (
-        <div key={idx} className='line'>
-          {line.map((item, idx2) => (
+      {Data().map((row, colIdx) => (
+        <div key={colIdx} className='column'>
+          {row.map((value, rowIdx) => (
             <div
-              key={idx2}
-              className={`item`}
-              style={{color: `hsl(${item * COLOR_RATIO}, 100%, ${idx2 < idx ? '92%' : '75%'})`}}
-            >{item || ''}</div>
+              key={rowIdx}
+              className={`value`}
+              style={{
+                color: GetColor(value, colIdx, rowIdx),
+              }}
+            >{value || ''}</div>
           ))}
         </div>
       ))}
